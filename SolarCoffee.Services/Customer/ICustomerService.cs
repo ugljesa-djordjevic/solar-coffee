1744a81a6c8a78f32d46958acad192ac02fa28a4
UgiBugi <ugljesa.s.djordjevic@gmail.com>
2021-06-19 18:31:50 +0200

    A bunch of new files

---
+++ SolarCoffee.Services/Customer/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Services.Customer
+{
+    public interface ICustomerService
+    {
+        public List<Data.Models.Customer> GetAllCustomers();
+        public ServiceResponse<Data.Models.Customer> CreateCustomer(Data.Models.Customer customer);
+        public ServiceResponse<bool> DeleteCustomer(int id);
+        public Data.Models.Customer GetById(int id);
+    }
+}
---
+++ SolarCoffee.Services/Inventory/InventoryService.cs
@@ -0,0 +1,16 @@
+﻿using SolarCoffee.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarCoffee.Services.Inventory
+{
+    public interface InventoryService
+    {
+        public List<ProductsInventory> GetCurrentInventory();
+        public ServiceResponse<ProductsInventory> UpdateUnitsAvailable(int id, int adjustment);
+        public ProductsInventory GetProductId(int productId);
+        public void CreateSnapshot();
+        public List<ProductsInventorySnapshot> GetSnapshotHistory();
+    }
+}
